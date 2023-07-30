a2a07103fc8d38b87cfb8fa796ce6b2ca2cb2819
Ahmed Ellashy <ahmadellashy2000@gmail.com>
2023-07-31 02:20:04 +0300

    added category and product model and views

---
+++ Zodiec/Model/CategoryModel.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryModel.swift
+//  Zodiec
+//
+//  Created by Ahmad Ellashy on 31/07/2023.
+//
+
+import UIKit
+
+class CategoryModel {
+    
+}
---
+++ Zodiec/Model/ProductModel.swift
@@ -0,0 +1,12 @@
+//
+//  ProductModel.swift
+//  Zodiec
+//
+//  Created by Ahmad Ellashy on 31/07/2023.
+//
+
+import UIKit
+
+class ProductModel {
+    
+}
