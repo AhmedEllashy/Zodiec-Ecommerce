1ff1a3c8a79316846fdcca86640ccb71c1edb5b3
Ahmed Ellashy <ahmadellashy2000@gmail.com>
2023-07-31 02:27:12 +0300

    added models

---
+++ Zodiec/Model/CategoryModel.swift
@@ -0,0 +1,8 @@
+//
+//  CategoryModel.swift
+//  Zodiec
+//
+//  Created by Ahmad Ellashy on 31/07/2023.
+//
+
+import Foundation
---
+++ Zodiec/Model/ProductModel.swift
@@ -0,0 +1,8 @@
+//
+//  ProductModel.swift
+//  Zodiec
+//
+//  Created by Ahmad Ellashy on 31/07/2023.
+//
+
+import Foundation
